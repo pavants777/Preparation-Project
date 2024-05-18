4f8680559449925909986d39f776dba83211d614
PavanGowdaTS <pavangowdats10@gmail.com>
2024-05-18 13:38:27 +0530

    First Update

---
+++ backend/Routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const authRoutes = express.Router();
+
+
+// POST /signup
+authRoutes.post('/signup', (req, res) => {
+    // Handle signup logic here
+});
+
+
+module.exports = authRoutes;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./Routes/auth');
+
+const app = express();
+app.use(express.json());
+app.use(authRoutes);
+
+const PORT = process.env.PORT || 3000;
+const DB = process.env.DB;
+
+mongoose.connect(DB)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
 
